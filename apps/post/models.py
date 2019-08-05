cc3c3424fab6568b1f1276d4935d383922720fc2
IgnisAvis <auswahlen.a@gmail.com>
2019-08-05 21:38:27 +0300

    Updated class Post

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+# Class for Post abstraction
 class Post(models.Model):
-   title = models.CharField(max_length=30)
-   url =  models.CharField(max_length=30)
+   title = models.CharField(max_length=30)      # Article title
+   url =  models.CharField(max_length=30)       # Url to original article
+   
+   # Image for article
    image = models.ImageField(upload_to='static/images/', blank=True, max_length=1000)
-   text = models.CharField(max_length=3000)
+   text = models.CharField(max_length=3000)     # Article main text
 
+    # Return title, when object used by name
+    def __str__(self):
+        return self.title
 
-class Meta:
-   abstract = True
+    # Class is abstact. Good for inheritance
+    class Meta:
+        abstract = True
