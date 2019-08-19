0960b2905ce62eb160af893afae2c8dfeb29c02a
IgnisAvis <auswahlen.a@gmail.com>
2019-08-19 12:55:01 +0300

    Revert "Fixed collect static error"
    This reverts commit 82254fa21b41097af4bae1f7d5f2b3a0bf884b64.

--- docker_update_db.sh
+++ docker_update_db.sh
@@ -3,4 +3,5 @@
 docker-compose up -d
 docker exec dpg_api python manage.py makemigrations authentication
 docker exec dpg_api python manage.py migrate --noinput
+docker exec dpg_api python manage.py collectstatic
 docker-compose stop
