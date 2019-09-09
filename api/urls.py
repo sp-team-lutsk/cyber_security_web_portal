4cf09089a0d2fb7544f3e5766c8d6beb26018e06
MARTIN <lvarchenko99@gmail.com>
2019-09-09 16:11:31 +0300

    url

--- api/urls.py
+++ api/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('api/v1/', include('authentication.urls')),
+    path('api/v1/auth/', include('authentication.urls')),
 ]  
 
 if settings.base.DEBUG == True:
