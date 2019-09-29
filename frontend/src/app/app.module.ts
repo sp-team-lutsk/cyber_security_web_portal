80ba20c8a8b72e7e3b21423c70857f2819a17355
joni2881 <dimapalonka3@gmail.com>
2019-09-29 20:44:41 +0300

    i have done header footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { AlertModule, ButtonsModule } from 'ngx-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
 })
+
 export class AppModule { }
