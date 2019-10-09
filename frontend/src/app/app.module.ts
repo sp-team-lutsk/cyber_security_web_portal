b7e3a6691aa196f776cafebe608267f70385dd16
joni2881 <dimapalonka3@gmail.com>
2019-10-09 18:11:32 +0300

    add routs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,31 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { AlertModule, ButtonsModule } from 'ngx-bootstrap';
-
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from './main/header/header.component';
+import { BodyComponent } from './main/body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import {StudentsComponent} from "./students/students.component";
+import {RouterModule} from "@angular/router";
+import { ScienceComponent } from './science/science.component';
+import { StructureComponent } from './structure/structure.component';
+import { AboutComponent } from './about/about.component';
+
+const routes = [
+  {path: '', component: BodyComponent},
+  {path: 'students', component: StudentsComponent},
+  {path: 'science', component: ScienceComponent},
+  {path: 'structure', component: StructureComponent},
+  {path: 'about', component: AboutComponent},
+]
 
 @NgModule({
   declarations: [
+    AppComponent,
     HeaderComponent,
     BodyComponent,
+    StudentsComponent,
     FooterComponent,
+    ScienceComponent,
+    StructureComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AlertModule.forRoot(),
-    ButtonsModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [
+    AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    StudentsComponent,
+    ScienceComponent,
+    StructureComponent,
+    AboutComponent,
+    FooterComponent,
   ],
 })
 
--- frontend/src/app/students/students-routing.module.ts
+++ frontend/src/app/students/students-routing.module.ts
@@ -4,9 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class StudentsRoutingModule { }
