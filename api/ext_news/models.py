b501c5b5981c4cb55ee3fc03c31a9c0b2c87cd11
IgnisAvis <auswhlen.a@gmail.com>
2019-10-11 18:26:24 +0300

    Fix models.py

--- api/ext_news/models.py
+++ api/ext_news/models.py
@@ -5,7 +5,7 @@ class News(models.Model):
     title = models.CharField(max_length=64, default="", blank=False)
     description = models.TextField(max_length=512, default="", blank=True)
     news_link = models.URLField(default="", blank=True)
-    images_link = models.ImageField(upload_to='ext_news', blank=True)
+    images_link = models.URLField(default="", blank=True)
     is_checked = models.BooleanField(default=False)
 
     def __str__(self):
