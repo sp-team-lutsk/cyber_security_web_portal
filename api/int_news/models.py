f0359a5a48cb6188aa9b4daadc8bd25d00f966a8
romaoksenyuk <roma.oksenuk@gmail.com>
2019-10-26 20:49:44 +0300

    upd models.py

--- api/int_news/models.py
+++ api/int_news/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class News_int(models.Model):
     title = models.CharField(max_length=64, default="", blank=False)
-    description = models.TextField(max_length=512, default="", blank=True)
-    news_link = models.URLField(default="", blank=True)
+    content = models.TextField(max_length=512, default="", blank=True)
     images_link = models.URLField(default="", blank=True)
+   #author = models.ForeignKey(User)
+    date_publication = models.DateTimeField(auto_now=True, auto_now_add=False)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
     is_checked = models.BooleanField(default=False)
 
     def __str__(self):
