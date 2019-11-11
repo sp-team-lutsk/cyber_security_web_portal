15255ba6b361370f02640602553ae904cd8c8b2b
IgnisAvis <auswhlen.a@gmail.com>
2019-11-11 10:14:44 +0200

    Fixed conflicts

--- api/int_news/urls.py
+++ api/int_news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostUpdInt, PostInt
 
 urlpatterns = [
-    path(r'<int:pk>/', PostUpdInt.as_view(), name='update'),
-    path(r'', PostInt.as_view(), name='list'),
+    path('<int:pk>/', PostUpdInt.as_view(), name='update'),
+    path('/', PostInt.as_view(), name='list'),
  ]
