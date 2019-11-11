60e9f4d704a8ee33606f746e498d1d80f6b88b3c
Azrael-git <alexandr.shypulin.study@gmail.com>
2019-11-11 14:55:54 +0200

    Fixed

--- docker_nosetests.sh
+++ docker_nosetests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf htmlcov/
-docker exec -it dpg_api coverage run --omit='*/migrations/*.py,*__init__*,wsgi.py,manage.py,*apps.py,*adapter.py' --source='.' manage.py test authentication ext_news int_news
+docker exec -it dpg_api coverage run --omit='*/migrations/*.py,*__init__*,wsgi.py,manage.py,*apps.py,*adapter.py' --source='.' manage.py test authentication.tests ext_news int_news
 docker exec -it dpg_api coverage report -m
 docker exec -it dpg_api coverage html
 docker cp dpg_api:/opt/docker_polls_group/api/htmlcov ./htmlcov
