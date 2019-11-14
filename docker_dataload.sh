8bc0d984f466f71bd0c3997061cd4edc35b3d7b8
Martinfree <lvarchenko99@gmail.com>
2019-11-14 21:39:55 +0200

    t letter insert

--- docker_dataload.sh
+++ docker_dataload.sh
@@ -2,5 +2,5 @@
 
 
 docker-compose up -d
-docker exec -it dpg_api python manage.py shell -c "from authenication.models import StdUser; User=StdUser(); User.email='user@example.com'; User.set_password('Admin123!'); User.is_active=True; User.name='test_User'; User.save()"
+docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; User=StdUser(); User.email='user@example.com'; User.set_password('Admin123!'); User.is_active=True; User.name='test_User'; User.save()"
 docker-compose stop
