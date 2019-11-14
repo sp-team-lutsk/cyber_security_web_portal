cb8db0af9f0c9a3592303251b56b7ca404f6acaa
dzhyhalyuk <dzhyhalyuky@gmail.com>
2019-11-14 22:04:26 +0200

    create moderator

--- docker_dataload.sh
+++ docker_dataload.sh
@@ -2,5 +2,11 @@
 
 
 docker-compose up -d
+
+docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; Moderator=StdUser(); Moderator.email='moderator@example.com'; Moderator.set_password('Admin123!'); Moderator.is_active=True; Moderator.is_moderator=True; Moderator.name='test_Moderator'; Moderator.save()"
 docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; User=StdUser(); User.email='user@example.com'; User.set_password('Admin123!'); User.is_active=True; User.name='test_User'; User.save()"
+
+
+
+
 docker-compose stop
