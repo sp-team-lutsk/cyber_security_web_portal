1cab39f77760e29a65c6d89b60dfd19075413598
dzhyhalyuk <dzhyhalyuky@gmail.com>
2019-11-14 21:34:56 +0200

    fix

--- docker_dataload.sh
+++ docker_dataload.sh
@@ -2,5 +2,5 @@
 
 
 docker-compose up -d
-docker exec -it dpg_api python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.email='user@example.com'; User.set_password(password='Admin123!'); User.is_active=True; User.name='test_User'; User.save()"
+docker exec -it dpg_api python manage.py shell -c "from authenication.models import StdUser; User=StdUser(); User.email='user@example.com'; User.set_password('Admin123!'); User.is_active=True; User.name='test_User'; User.save()"
 docker-compose stop
