eaac0e25313cd567b0b7078bcdedbdf300130c96
dzhyhalyuk <dzhyhalyuky@gmail.com>
2019-11-14 22:15:17 +0200

    add teacher

--- docker_dataload.sh
+++ docker_dataload.sh
@@ -6,6 +6,8 @@ docker-compose up -d
 docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; Moderator=StdUser(); Moderator.email='moderator@example.com'; Moderator.set_password('Admin123!'); Moderator.is_active=True; Moderator.is_moderator=True; Moderator.name='test_Moderator'; Moderator.save()"
 docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; User=StdUser(); User.email='user@example.com'; User.set_password('Admin123!'); User.is_active=True; User.name='test_User'; User.save()"
 
+docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; Teacher=StdUser();Teacher.email='teacher@example.com'; Teacher.set_password('Admin123!'); Teacher.is_active=True; Teacher.is_teacher=True; Teacher.name='test_Teacher'; Teacher.save()"
+
 
 
 
