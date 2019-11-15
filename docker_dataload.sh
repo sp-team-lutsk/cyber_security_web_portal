771e22a3daf77cad2562d30986bf67f26b305767
Martinfree <lvarchenko99@gmail.com>
2019-11-15 21:07:05 +0200

    images_link fix

--- docker_dataload.sh
+++ docker_dataload.sh
@@ -12,7 +12,7 @@ docker exec -it dpg_api python manage.py shell -c "from authentication.models im
 docker exec -it dpg_api python manage.py shell -c "from authentication.models import StdUser; Student=StdUser();Student.email='student@example.com'; Student.set_password('Admin123!'); Student.is_active=True; Student.is_student=True; Student.name='test_Student'; Student.save()"
 
 
-docker exec -it dpg_api python manage.py shell -c "from ext_news.models import News; news=News(); news.title='Test_News_1'; news.description='description'; news.news_link='localhost/'; news.images_link=None; news.is_checked=True; news.save()"
+docker exec -it dpg_api python manage.py shell -c "from ext_news.models import News; news=News(); news.title='Test_News_1'; news.description='description'; news.news_link='localhost/'; news.images_link='localhost/'; news.is_checked=True; news.save()"
 
 
 docker-compose stop
