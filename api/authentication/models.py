0c4925ad63090bcbcae9fc8d51f4bfbb0f92c318
Azrael-git <alexandr.shypulin.study@gmail.com>
2019-11-16 16:57:53 +0200

    Models fix

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -42,13 +42,6 @@ class StdUserManager(UserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        return self._create_user(email, password, **extra_fields)
-
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
