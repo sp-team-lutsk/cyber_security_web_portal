287b9588f6e99e2644a26414acb24302011a7371
romaoksenyuk <roma.oksenuk@gmail.com>
2019-11-17 14:27:16 +0200

    change pk to id

--- api/ext_news/urls.py
+++ api/ext_news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostUpd, Post, MailingAPIView
 
 urlpatterns = [
-    path('<int:pk>', PostUpd.as_view(), name='upd'),
+    path('<int:id>', PostUpd.as_view(), name='upd'),
     path('', Post.as_view(), name=None),
     path('mailing/',MailingAPIView.as_view(), name='mailing'),
 ]
--- api/ext_news/views.py
+++ api/ext_news/views.py
@@ -17,27 +17,27 @@ class PostUpd(APIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
-    def get_object(self, pk):
+    def get_object(self, id):
         try:
-            return News.objects.get(pk=pk)
+            return News.objects.get(id=id)
         except News.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        news = self.get_object(pk)
+    def get(self, request, id):
+        news = self.get_object(id)
         serializer = NewsSerializer(news)
         return Response(serializer.data)
 
-    def put(self, request, pk):
-        news = self.get_object(pk)
+    def put(self, request, id):
+        news = self.get_object(id)
         serializer = NewsSerializer(news, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        news = self.get_object(pk)
+    def delete(self, request, id):
+        news = self.get_object(id)
         news.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
