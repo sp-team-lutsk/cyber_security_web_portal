4288aad20ed2cc14a985ee15d43f83a5946f1f75
Azrael-git <alexandr.shypulin.study@gmail.com>
2019-11-22 19:02:36 +0200

    New structure added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,60 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from './main/header/header.component';
-import { BodyComponent } from './main/body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { StudentsComponent } from "./students/students.component";
-import { RouterModule } from "@angular/router";
-import { ScienceComponent } from './science/science.component';
-import { AboutComponent } from './about/about.component';
-import { MailingComponent } from './mailing/mailing.component';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { LogInComponent } from './components/log-in/log-in.component';
-
-const routes = [
-      {path: '', component: BodyComponent},
-      {path: 'students', component: StudentsComponent},
-      {path: 'science', component: ScienceComponent},
-      {path: 'about', component: AboutComponent},
-      {path: 'mailing', component: MailingComponent}
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    StudentsComponent,
-    FooterComponent,
-    ScienceComponent,
-    AboutComponent,
-    MailingComponent,
-    RegistrationComponent,
-    LogInComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
-    FontAwesomeModule,
-    BrowserAnimationsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [
-    AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    StudentsComponent,
-    ScienceComponent,
-    AboutComponent,
-    MailingComponent,
-    FooterComponent,
-  ],
+  bootstrap: [AppComponent]
 })
-
 export class AppModule { }
