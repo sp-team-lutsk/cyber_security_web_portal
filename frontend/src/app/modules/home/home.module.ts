8a6331452989ecc68e4822250c8cae00f2acb81f
joni2881 <dimapalonka3@gmail.com>
2019-11-25 15:52:10 +0200

    refresh

--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { ScienceComponent } from './pages/science/science.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MainComponent } from './pages/main/main.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, StudentsComponent, ScienceComponent, AboutComponent, MainComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
