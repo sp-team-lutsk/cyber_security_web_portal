6d096435f03fcc5764ab8402278bb7962aa231c9
Yura <krulikyura228@gmail.com>
2019-12-06 22:08:42 +0200

    'add_Charts'

--- frontend/src/app/modules/home/pages/science/science.component.ts
+++ frontend/src/app/modules/home/pages/science/science.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
 import { faAngleDown, faAngleUp, faChartLine, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,10 +13,109 @@ export class ScienceComponent implements OnInit {
   faAngleUp = faAngleUp;
   faChartLine = faChartLine;
   faEdit = faEdit;
+  LineChart=[];
 
   constructor() { }
 
   ngOnInit() {
+    //Графік відвідуваності
+    this.LineChart = new Chart('lineChartVisit', {
+
+     type: 'line',
+
+    data: {
+    labels: ["вересень", "жовтень", "листопад", "грудень", "січень", "лютий","березень","квітень","травень","червень"],
+    datasets: [{
+        label: 'Відвідуваність',
+        data: [10,30 , 19, 30, 40, 42,45,68,78],
+        pointRadius: 0,
+        fill:false,
+        lineTension:0,
+        borderColor:"rgb(78, 188, 210)"
+
+    }]
+    },
+    options: {
+      responsive: false,
+      legend: {
+        display: false,
+    },
+
+    tooltips:{
+        backgroundColor: "white",
+        titleFontColor: "black",
+        bodyFontColor: "black",
+        footerFontColor: "black"
+
+        },
+    scales: {
+        yAxes: [{
+            gridLines: {
+              color:"black"
+            },
+            ticks: {
+                beginAtZero:true
+            }
+        }],
+        xAxes: [{
+
+          gridLines: {
+            drawTicks:true,
+            display:false
+          }
+        }]
+    }
+    }
+    });
+
+    //Графік успішності
+    this.LineChart = new Chart('lineChartSuccess', {
+
+     type: 'line',
+
+    data: {
+    labels: ["вересень", "жовтень", "листопад", "грудень", "січень", "лютий","березень","квітень","травень","червень"],
+    datasets: [{
+        label: 'Успішність',
+        data: [10,30 , 19, 30, 40, 42,45,68,78],
+        pointRadius: 0,
+        fill:false,
+        lineTension:0,
+        borderColor:"rgb(78, 188, 210)"
+    }]
+    },
+    options: {
+      responsive: false,
+      legend: {
+        display: false,
+    },
+
+      tooltips:{
+            backgroundColor: "white",
+            titleFontColor: "black",
+            bodyFontColor: "black",
+            footerFontColor: "black"
+
+        },
+    scales: {
+        yAxes: [{
+            gridLines: {
+              color:"black"
+            },
+            ticks: {
+                beginAtZero:true
+            }
+        }],
+        xAxes: [{
+
+          gridLines: {
+            drawTicks:true,
+            display:false
+          }
+        }]
+    }
+    }
+    });
   }
 
 }
