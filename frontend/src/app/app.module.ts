289db87d912d21267b48972d99e4fbd906b5ea40
Yura <krulikyura228@gmail.com>
2019-12-08 13:33:29 +0200

    fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-<<<<<<< HEAD
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
-=======
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from './main/header/header.component';
-import { BodyComponent } from './main/body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { StudentsComponent } from "./students/students.component";
-import { RouterModule } from "@angular/router";
-import { ScienceComponent } from './science/science.component';
-import { AboutComponent } from './about/about.component';
-import { MailingComponent } from './mailing/mailing.component';
 
->>>>>>> master
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 
@@ -27,15 +16,11 @@ import { DashboardModule } from './modules/dashboard/dashboard.module';
   imports: [
     HomeModule,
     BrowserModule,
-<<<<<<< HEAD
+
     AppRoutingModule,
     FontAwesomeModule,
     DashboardModule,
-=======
-    RouterModule.forRoot(routes),
-    FontAwesomeModule,
-    BrowserAnimationsModule,
->>>>>>> master
+
   ],
   providers: [],
   bootstrap: [AppComponent]
