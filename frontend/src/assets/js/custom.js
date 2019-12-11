ecb50b36344308f4dd97c48894ed039d357c2230
joni2881 <amuvor11@gmail.com>
2019-12-11 19:43:01 +0200

    structure

--- frontend/src/assets/js/custom.js
+++ frontend/src/assets/js/custom.js
@@ -1,3 +1,13 @@
+window.onload = function() {
+
+  setTimeout(function() {
+    var preloader = document.getElementById('page-preloader')
+    if ( !preloader.classList.contains('done') ) {
+      preloader.classList.add('done');
+    }
+  }, 1000);
+
+}
 function readMore() {
   let more = document.getElementById("more");
   let btn = document.getElementById("btn");
